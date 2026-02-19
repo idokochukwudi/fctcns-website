b9952c4f94e97d6c721a5cc97523ace81cb3c2e6
idokochukwudie <idokochukwudie@gmail.com>
2026-02-19 18:37:30 +0100

    kkkklll

--- app/views/applications/step3.php
+++ app/views/applications/step3.php
@@ -850,6 +850,6 @@ if (empty($applicant_name)) {
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/js/Payment.js"></script>
+<script src="/assets/js/payment.js"></script>
 </body>
 </html>
