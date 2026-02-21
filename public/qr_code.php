71a7b3c6c03c05b635ff2a82e2c5805370c19dd1
idokochukwudie <idokochukwudie@gmail.com>
2026-02-21 11:52:17 +0100

    teseeee

--- public/qr_code.php
+++ public/qr_code.php
@@ -1,12 +1,17 @@
 <?php
+/**
+ * Simple QR Code Generator using QR Server API
+ */
+
 $slipNumber = $_GET['slip'] ?? 'SLIP-2025-00001';
 $slipNumber = preg_replace('/[^A-Za-z0-9\-]/', '', $slipNumber);
 
-// Point to your verification page
-$verificationUrl = 'http://fctcns.edu.ng/verify.php?slip=' . urlencode($slipNumber);
+// Use a reliable QR code API
+$verificationUrl = 'http://fctcns.edu.ng/test_verify.php?slip=' . urlencode($slipNumber);
 
-// Use QR Server API
+// Use QR Server API (free, no API key required)
 $qrApiUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=' . urlencode($verificationUrl);
 
+// Redirect to the QR server
 header('Location: ' . $qrApiUrl);
 exit;
